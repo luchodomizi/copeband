11548a5454684caee7718bd45d5ca20d17e26f20
luchodomizi <luchodomizi@live.com>
2021-02-11 21:31:12 -0300

    first commit

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector('.menu')
+const navegacion = document.querySelector('.navegacion')
+
+console.log(navegacion)
+console.log(menu)
+
+
+menu.addEventListener('click', ()=>{
+    navegacion.classList.toggle("spread")
+})
+
+window.addEventListener('click', e =>{
+    if(navegacion.classList.contains('spread') 
+        && e.target != navegacion && e.target != menu){
+            console.log('cerrar')
+            navegacion.classList.toggle("spread")
+        }
+
+})
